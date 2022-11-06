a55ea86d955e137213ee72d9974de5316fae774d
tonezone6 <tonezone6@gmail.com>
2022-11-06 14:26:17 +0100

    Simple alert

---
+++ Sources/swiftui-extensions/SimpleAlert.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Alex S. on 06/11/2022.
+//
+
+import SwiftUI
+
+extension View {
+    public func simpleAlert<E>(error: Binding<E?>, title: String = "Alert") -> some View
+    where E: Error, E: Identifiable, E: LocalizedError {
+        self.alert(item: error) { error in
+            Alert(
+                title: Text(title),
+                message: Text(error.errorDescription ?? ""),
+                dismissButton: .cancel()
+            )
+        }
+    }
+}
